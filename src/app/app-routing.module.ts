abd5b69f142e372000b52a5faf338b0b78b09e6a
Prabhat <prabhatchindarkar1998@gmail.com>
2021-01-13 12:00:57 +0530

    minor
    changes

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,16 +1,18 @@
-import { NgModule } from '@angular/core';
+import { Component, NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { EditUserComponent } from './component/edit-user/edit-user.component';
 import { HomeComponent } from './component/home/home.component';
 import { UserRegistrationComponent } from './component/user-registration/user-registration.component';
 import { RegistrationComponent } from './component/registration/registration.component';
+import { ProfileComponent } from './component/profile/profile.component';
 
 
 const routes: Routes = [
   {path:'registration/:id', component:RegistrationComponent},
   {path:'userRegister' ,component:UserRegistrationComponent},
   {path:'edit/:id', component:EditUserComponent},
-  {path:'', component:HomeComponent}
+  {path:'', component:HomeComponent},
+  {path:'profile',component:ProfileComponent}
   // { path: 'list', component: ListEmployeesComponent },
   // { path: 'create', component: CreateEmployeeComponent },
   // { path: 'edit/:id', component: CreateEmployeeComponent },
